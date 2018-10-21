9339a217afa8da1b72ce4ef8a8b81604ecf241d4
Sweet Ace <lakshmialeman@gmail.com>
2018-10-20 23:05:55 -0700

    added the do what it says function, and finished documentation clean up.

--- liri.js
+++ liri.js
@@ -3,16 +3,13 @@ require("dotenv").config();
 
 //access keys from the file
 var keys = require("./keys");
-//var keys = require("./keys.js")
 
-// Include the request npm package, node-spotify-api, moment
+// Include the request npm package, node-spotify-api, moment, fs
 //(Don't forget to run "npm install request", "npm install node-spotify-api", "npm install moment" in this folder first!)
 var request = require("request");
 var Spotify = require("node-spotify-api");
 var moment = require("moment");
-
-//tests current date
-//console.log (currMoment);
+var fs = require("fs");
 
 //creates a new spotify key
 var spotify = new Spotify(keys.spotify);
@@ -23,67 +20,82 @@ var omdbapi = keys.omdb;
 //creates a new bands in town api key
 var bandsInTown = keys.bandsInTown;
 
+//input from the user
 var command = process.argv[2];
-//console.log(command);
-// Grab the movieName, concert, song, do what it says command which will always be the third node argument.
+
+// Grab the movieName, concert, song, do-what-it-says command which will always be the third node argument and assign it to an array.
 var nameArr = process.argv.slice(3);
-//console.log("nameARR join"+nameArr.join(" "));
-var commandCall = (nameArr).join("");
-//console.log(commandCall);
-//for example the commands will be read as such
+
+//join the elements of the array to a string
+var commandCall = (nameArr).join(" ");
+
+
 //will be able to hold the following commands
 //concert-this, spotify-this-song, movie-this, do-what-it-says
-switch (command) {
-    case "concert-this":
-        bandsInTownCall();
-        break;
-    case "spotify-this-song":
-        spotifyApi();
-        break;
-    case "movie-this":
-        omdbCall();
-        break;
-    case "do-what-it-says":
-        break;
+function choose(inputCommand) {
+    //choose what command to run
+    switch (inputCommand) {
+        case "concert-this":
+            bandsInTownCall();
+            break;
+        case "spotify-this-song":
+            spotifyApi();
+            break;
+        case "movie-this":
+            omdbCall();
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+    }
 }
+
+//runs the commands themselves
+choose(command);
+
 //calls and returns omdbapi data
 function omdbCall() {
-    //handle space conversion to + for commandCall
+    //handles the case where the user doensn't provide a movie
+    if (commandCall === "") {
+        //default movie
+        commandCall = "Mr. Nobody";
+    }
+
     // Then run a request to the OMDB API with the movie specified
     var ombdUrl = "http://www.omdbapi.com/?t=" + commandCall + "&y=&plot=short&apikey=" + omdbapi.apikey;
 
-    //concert-this <artist/band name here>
-    console.log(ombdUrl);
-
+    //request movie to the api
     request(ombdUrl, function (error, response, body) {
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-            // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+        // initialize the data to be accessed
+        let movieApiData = JSON.parse(body);
 
+        // If the request is successful
+        if (!error && response.statusCode === 200 && movieApiData.Error === undefined) {
             //* Title of the movie.
-            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Title: " + movieApiData.Title);
             //* Year the movie came out.
-            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("Release Year: " + movieApiData.Year);
             //* IMDB Rating of the movie.
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("IMDB Rating: " + movieApiData.imdbRating);
             //* Rotten Tomatoes Rating of the movie.
-            console.log(JSON.parse(body).Ratings[1].Source + ": " + JSON.parse(body).Ratings[1].Value);
+            console.log(movieApiData.Ratings[1].Source + ": " + movieApiData.Ratings[1].Value);
             //* Country where the movie was produced.
-            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Country: " + movieApiData.Country);
             //* Language of the movie.
-            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Language: " + movieApiData.Language);
             //* Plot of the movie.
-            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Plot: " + movieApiData.Plot);
             //* Actors in the movie.
-            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("Actors: " + movieApiData.Actors);
+        } else {
+            //handles wrong movie input
+            console.log(movieApiData.Error);
         }
     });
 }
 
 //calls and returns bandsInTown data
 function bandsInTownCall() {
-    //handle space conversion to + for commandCall
     // Then run a request to the bandsintown API with the artist specified
     var bandsInTownUrl = "https://rest.bandsintown.com/artists/" + commandCall + "/events?app_id=" + bandsInTown.app_id + "&date=upcoming";
 
@@ -119,8 +131,14 @@ function bandsInTownCall() {
 
 //calls and returns spotify data
 function spotifyApi() {
-    //converts the array that holds the data input from the user to as string
-    nameArr = nameArr.join(" ");
+    //handles no song input
+    if (nameArr.length !== 0) {
+        //converts the array that holds the data input from the user to as string becoming the song name
+        nameArr = nameArr.join(" ");
+    } else {
+        //pass in the following song in case no song was passed in
+        nameArr = "The Sign";
+    }
 
     //will hold the names of the artists
     var spotifyNames = [];
@@ -139,13 +157,14 @@ function spotifyApi() {
         let spotifyData = data.tracks.items;
 
         //loops through data set of 20 call backs
-         for (let key in spotifyData) {
+        for (let key in spotifyData) {
+            //initializes artist
             var artist = spotifyData[key].album.artists;
-            //output name of the song
+            //initializes name of the song
             var songName = spotifyData[key].name;
 
             //make sure the name of the songs matches to the results from the api exactly
-            if(songName.toLowerCase().search(nameArr.toLowerCase()) === -1){
+            if (songName.toLowerCase().search(nameArr.toLowerCase()) === -1) {
                 //keeps the loop going
                 continue;
             }
@@ -158,9 +177,9 @@ function spotifyApi() {
 
             //initialize list of artists
             let artistList = [];
-            
+
             //loop through array of artist
-            for(let innerKey in artist){
+            for (let innerKey in artist) {
                 //initialize artist name
                 var artistName = artist[innerKey].name;
 
@@ -178,18 +197,18 @@ function spotifyApi() {
             };
         }
         //loops through the data set of artists
-        for(let keyName in nameSet){
+        for (let keyName in nameSet) {
             //adds the data 
             spotifyNames.push(
                 //add the name of the artist
-                "Artist: " + keyName + 
+                "Artist: " + keyName +
 
                 //add the name of the song
                 "\nSong's Name: " + nameSet[keyName].songName +
 
                 //add the url to the spotify song
                 "\nPreview Song in Spotify: " + nameSet[keyName].previewLinkSong +
-                
+
                 //add the name of the album
                 "\nAlbum's Name: " + nameSet[keyName].albumName +
 
@@ -198,5 +217,33 @@ function spotifyApi() {
         }
         //output the data
         console.log(spotifyNames.join("\n"));
+    });
+}
+
+//runs the data file random.txt
+function doWhatItSays() {
+    // This block of code will read from the "random.txt" file.
+    // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+    // The code will store the contents of the reading inside the variable "data"
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // We will then print the contents of data
+        console.log(data);
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+
+        //remove quotation marks from the data comming from the file and initialize the nameArr array from the elements that contain spaces in between
+        nameArr = dataArr[1].replace("\"", "").split(" ");
+
+        //initialize
+        commandCall = nameArr.join(" ");
+        //send commands to choose from
+        choose(dataArr[0]);
     });
 }
